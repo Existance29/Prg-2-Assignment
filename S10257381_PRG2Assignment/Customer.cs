71d46c55368ea811214ed142d7d84f732f682c90
donovan1025 <S10257381@connect.np.edu.sg>
2024-01-10 13:48:06 +0800

    Added Customer.cs and PointCard.cs

---
+++ S10257381_PRG2Assignment/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10257381_PRG2Assignment
+{
+    class Customer: Order
+    {
+        //Properties
+
+        public string name { get; set; }
+
+        public int memberid { get; set; }
+
+        public DateTime dob { get; set; }
+
+        public Order currentOrder { get; set; }
+
+        public List<Order> orderHistory { get; set; }
+
+        public PointCard rewards { get; set; }
+
+        //Constructors
+
+        public Customer() { }
+
+        public Customer(string n,int mid,DateTime d)
+        {
+            name = n;
+            memberid = mid;
+            dob = d;
+        }
+
+        public Order MakeOrder()
+        {
+
+        }
+
+        public bool Isbirthday()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ S10257381_PRG2Assignment/PointCard.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10257381_PRG2Assignment
+{
+    class PointCard:Customer
+    {
+        //Properties
+        public int points { get; set; }
+
+        public int punchCard { get; set; }
+
+        public string tier { get; set; }
+
+        //Constructors
+
+        public PointCard() { }
+
+        public PointCard(int p, int pc)
+        {
+            points = p;
+            punchCard = pc;
+        }
+
+        public void AddPoints(int points)
+        {
+            
+        }
+
+        public void RedeemPoints(int points)
+        {
+
+        }
+
+        public void Punch()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
